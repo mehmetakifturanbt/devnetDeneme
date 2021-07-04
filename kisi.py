ad169a72b7ad504c6af7ed21169dd6b18f553930
mehmetakifturanbt <mehmetakifturan@gmail.com>
2021-07-04 21:38:13 +0000

    ilk commit

---
+++ kisi.py
@@ -0,0 +1,11 @@
+class Kisi:
+	def __init__(self,ad,yas):
+		self.ad = ad
+		self.yas = yas
+	
+	def karsilama(self):
+		print("Hoş geldin ", self.ad)
+
+kisi1 = Kisi("Cahit", 35)
+kisi1.karsilama()
+
